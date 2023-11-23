a2511b1d9de69436e50a79011e2ebe2d868cd165
2901shivam <130030611+2901shivam@users.noreply.github.com>
2023-11-23 16:08:06 +0530

    Add files via upload

---
+++ Triangle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaLib
+{
+    public class Triangle
+    {
+        public static int Area(int x ,int y)
+        {
+            int result = 1 / 2*(x * y);
+            return result;
+
+        }
+    }
+}
