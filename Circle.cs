37a48bd3ea6144011a7f7e3b36b66f3f3351f61b
2901shivam <130030611+2901shivam@users.noreply.github.com>
2023-11-23 16:06:49 +0530

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaLib
+{
+    public class Circle
+    {
+        public static double Area(int r)
+        {
+            double result = 3.14 * r * r;
+            return result;
+
+        }
+    }
+}
