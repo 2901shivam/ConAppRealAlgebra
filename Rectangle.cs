4748bdf6c6181a0c2da9f78998064a25439fadf6
2901shivam <130030611+2901shivam@users.noreply.github.com>
2023-11-23 16:07:07 +0530

    Add files via upload

---
+++ Rectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ShapeAreaLib
+{
+    public class Rectangle
+    {
+      
+        public static int Area(int x,int y)
+        {
+            int result = x * y;
+            return result;
+        }
+        
+        
+        
+    }
+}
